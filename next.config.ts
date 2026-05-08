440aa62d491f2f4c6c288fcc12c0ccbe73a6bc4d
usame132 <ussama14863@gmail.com>
2026-05-08 21:07:29 +0300

    seconed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: true, // Enable Turbopack
+  },
 };
 
 export default nextConfig;
